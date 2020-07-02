f6a44012e0bb1088b2d9ca395ac55ff0364720c1
ckthsen <ckthsen@126.com>
2020-07-02 15:25:03 +0800

    update initial_setup.sh

--- macos/initial_setup.sh
+++ macos/initial_setup.sh
@@ -8,7 +8,7 @@ brew cask install iterm2
 brew install zsh  tmux neovim 
 
 # Install zplug if required
-! [[ -d $HOME/.zplug ]] && curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+ ! [[ -d $HOME/.zplug ]] && curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -20,10 +20,11 @@ export TMUX_HOME=$HOME/.tmux
 # Install plug for neovim
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Put dotfiles to places
+ Put dotfiles to places
 DOTFILES_BASE=https://raw.githubusercontent.com/akind3/dotfiles/master/macos
+rm -f $HOME/{.zshrc, .tmux.conf, .nvimrc, .aliases} 
 curl -fLo $HOME/.zshrc --create-dirs  $DOTFILES_BASE/zshrc
 curl -fLo $HOME/.tmux.conf --create-dirs  $DOTFILES_BASE/tmux.conf
 curl -fLo $HOME/.config/nvim/init.vim --create-dirs  $DOTFILES_BASE/nvimrc
-ln -s $HOME/.config/nvim/init.vim  ~/.nvimrc
+ln -s $HOME/.config/nvim/init.vim  $HOME/.nvimrc
 curl -fLo $HOME/.aliases --create-dirs  $DOTFILES_BASE/aliases
