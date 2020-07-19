3d3b70c1722f01975a320c5253b43ac12a6180a5
akind3 <akinthsen@gmail.com>
2020-07-19 21:57:27 +0800

    add pyenv and profile

--- macos/zsh/pyenv.zsh
+++ macos/zsh/pyenv.zsh
@@ -1,2 +1,5 @@
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+# if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+# if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
