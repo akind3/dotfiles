d0e3b994f578df99a74cf2068a748905db3de61b
akind3 <akinthsen@gmail.com>
2021-04-28 22:13:48 +0800

    change task to use system to run

--- win10/时间同步/create_tasks.bat
+++ win10/时间同步/create_tasks.bat
@@ -1,5 +1,6 @@
 @Rem 创建 w32tm 服务配置的定时任务
-schtasks /create /sc onstart /rl highest /tn "windows_time_service_setting" /tr c:\w32time_setting.bat
+@Rem schtasks /create /sc onstart /rl highest /ru system /tn "windows_time_service_setting" /tr c:\w32time_setting.bat
+schtasks /create /sc onstart /ru system /tn "windows_time_service_setting" /tr c:\w32time_setting.bat
 echo "task windows_time_service_setting created..."
 
 @Rem  wait 3 seconds
@@ -7,7 +8,8 @@ ping 129.0.0.1 -n 1 -w 3000 > nul
 
 
 @Rem 创建 w32tm /resync 定时任务
-schtasks /create /sc minute /mo 30 /rl highest /tn "windows_time_sync" /tr c:\w32time_sync.bat 
+@Rem schtasks /create /sc minute /mo 2 /rl highest /ru system /tn "windows_time_sync" /tr c:\w32time_sync.bat 
+schtasks /create /sc minute /mo 2 /ru system /tn "windows_time_sync" /tr c:\w32time_sync.bat 
 echo "task windws_time_sync created..."
 
 
